d88a19c4f03a6214eb27a72b130e7afcbcae5e61
litefeel <litefeel@gmail.com>
2017-09-12 17:51:37 +0800

    update unity amd64-2017.1.0xf1Linux.deb

--- get-unity.sh
+++ get-unity.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-URL=http://download.unity3d.com/download_unity/linux/
-PKG=unity-editor-5.4.0p1+20160810_amd64.deb
+URL=http://beta.unity3d.com/download/427038f4b57e/
+PKG=unity-editor_amd64-2017.1.0xf1Linux.deb
 
 echo "Downloading Unity3D installer..."
 curl -o /app/unity_editor.deb -s "${URL}${PKG}" 
